33572d6d96e758c79306c9b0c36642b017aa3cc7
vb <vb@Rezas-iMac.fritz.box>
2024-01-19 01:02:25 +0100

    Initial commit

---
+++ MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC.Properties
+{
+    public class CategoriesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Edit( int? id)
+        {
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            return View(category);
+        }
+    }
+}
+
---
+++ MVC/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MVC.Models
+{
+	public class Category
+	{
+		public int  CategoryId { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+    }
+}
+
